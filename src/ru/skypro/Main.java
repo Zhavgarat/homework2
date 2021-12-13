4fde797ac1fe0aac62f1ea419a1fdd808819252b
saypulaeva.zs <s.zhavgarat@mail.ru>
2021-12-13 22:31:38 +0300

    Попытка 4

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -17,16 +17,15 @@ public class Main {
         if (clientOS == 1) {
             if (clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
-
             } else
                 System.out.println("Установите версию приложения для Android по ссылке");
         } else if (clientOS == 0) {
             if (clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-
             } else
                 System.out.println("Установите версию приложения для iOS по ссылке");
         }
+
         //3
         int year = 2021;
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
