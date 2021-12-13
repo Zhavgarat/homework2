a6799d56c0fbae91db9c211beef0bc52f3e859b6
saypulaeva.zs <s.zhavgarat@mail.ru>
2021-12-13 22:28:37 +0300

    Попытка 4

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -29,7 +29,6 @@ public class Main {
         }
         //3
         int year = 2021;
-
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
             System.out.println(year + " год является високосным");
         else {
